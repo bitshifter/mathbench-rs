222d301889548b05b873af2b9f9855c7ff3ef2e2
Cameron Hart <cameron.hart@gmail.com>
2019-05-10 15:59:36 +1000

    Update Cargo.lock, update macros

--- benches/mat4bench.rs
+++ benches/mat4bench.rs
@@ -1,77 +1,30 @@
+#[path = "support/macros.rs"]
+#[macro_use]
+mod macros;
 use criterion::{criterion_group, criterion_main};
-
-macro_rules! bench_unop {
-    ($name: ident, $desc: expr, $t: ty, $unop: ident) => {
-        pub(crate) fn $name(c: &mut Criterion) {
-            use rand::{Rng, SeedableRng};
-            use rand_xoshiro::Xoshiro256Plus;
-            const LEN: usize = 1 << 13;
-
-            let mut rng = Xoshiro256Plus::seed_from_u64(0);
-
-            let elems: Vec<$t> = (0..LEN).map(|_| rng.gen::<$t>()).collect();
-            let mut i = 0;
-
-            c.bench_function($desc, move |b| {
-                b.iter(|| {
-                    i = (i + 1) & (LEN - 1);
-
-                    unsafe {
-                        criterion::black_box(elems.get_unchecked(i).$unop())
-                    }
-                })
-            });
-        }
-    };
-}
-
-macro_rules! bench_func1 {
-    ($name: ident, $desc: expr, $t: ty, $func1: ident) => {
-        pub(crate) fn $name(c: &mut Criterion) {
-            use rand::{Rng, SeedableRng};
-            use rand_xoshiro::Xoshiro256Plus;
-            const LEN: usize = 1 << 13;
-
-            let mut rng = Xoshiro256Plus::seed_from_u64(0);
-
-            let elems: Vec<$t> = (0..LEN).map(|_| rng.gen::<$t>()).collect();
-            let mut i = 0;
-
-            c.bench_function($desc, move |b| {
-                b.iter(|| {
-                    i = (i + 1) & (LEN - 1);
-
-                    unsafe {
-                        criterion::black_box($func1(elems.get_unchecked(i)))
-                    }
-                })
-            });
-        }
-    };
-}
 
 mod bench_cgmath {
     use criterion::Criterion;
     use cgmath::{prelude::*, Matrix4};
-    bench_unop!(mat4_transpose, "cgmath mat4 transpose",  Matrix4<f32>, transpose);
-    bench_unop!(mat4_determinant, "cgmath mat4 determinant",  Matrix4<f32>, determinant);
-    bench_unop!(mat4_inverse, "cgmath mat4 inverse",  Matrix4<f32>, invert);
+    bench_unop!(mat4_transpose, "cgmath mat4 transpose", op => transpose, ty => Matrix4<f32>);
+    bench_unop!(mat4_determinant, "cgmath mat4 determinant", op => determinant, ty => Matrix4<f32>);
+    bench_unop!(mat4_inverse, "cgmath mat4 inverse",  op => invert, ty => Matrix4<f32>);
 }
 
 mod bench_glam {
     use criterion::Criterion;
     use glam::f32::Mat4;
-    bench_unop!(mat4_transpose, "glam mat4 transpose",  Mat4, transpose);
-    bench_unop!(mat4_determinant, "glam mat4 determinant",  Mat4, determinant);
-    bench_unop!(mat4_inverse, "glam mat4 inverse",  Mat4, inverse);
+    bench_unop!(mat4_transpose, "glam mat4 transpose",  op => transpose, ty => Mat4);
+    bench_unop!(mat4_determinant, "glam mat4 determinant",  op => determinant, ty => Mat4);
+    bench_unop!(mat4_inverse, "glam mat4 inverse",  op => inverse, ty => Mat4);
 }
 
 mod bench_nalgebra {
     use criterion::Criterion;
     use nalgebra_glm::{determinant, inverse, transpose, Mat4};
-    bench_func1!(mat4_transpose, "nalgebra mat4 transpose",  Mat4, transpose);
-    bench_func1!(mat4_determinant, "nalgebra mat4 determinant",  Mat4, determinant);
-    bench_func1!(mat4_inverse, "nalgebra mat4 inverse",  Mat4, inverse);
+    bench_func!(mat4_transpose, "nalgebra mat4 transpose",  op => transpose, ty => Mat4);
+    bench_func!(mat4_determinant, "nalgebra mat4 determinant",  op => determinant, ty => Mat4);
+    bench_func!(mat4_inverse, "nalgebra mat4 inverse",  op => inverse, ty => Mat4);
 }
 
 criterion_group!(
---
+++ benches/support/macros.rs
@@ -0,0 +1,97 @@
+#[macro_export]
+macro_rules! bench_func {
+    ($name: ident, $desc: expr, op => $func: ident, ty => $t: ty, from => $from: ty) => {
+        pub(crate) fn $name(c: &mut Criterion) {
+            use rand::{Rng, SeedableRng};
+            use rand_xoshiro::Xoshiro256Plus;
+            const LEN: usize = 1 << 13;
+
+            let mut rng = Xoshiro256Plus::seed_from_u64(0);
+
+            let elems: Vec<$t> = (0..LEN).map(|_| rng.gen::<$from>().into()).collect();
+            let mut i = 0;
+
+            c.bench_function($desc, move |b| {
+                b.iter(|| {
+                    i = (i + 1) & (LEN - 1);
+
+                    unsafe { criterion::black_box($func(elems.get_unchecked(i))) }
+                })
+            });
+        }
+    };
+
+    ($name: ident, $desc: expr, op => $func: ident, ty => $t: ty) => {
+        bench_func!($name, $desc, op => $func, ty => $t, from => $t);
+    };
+}
+
+#[macro_export]
+macro_rules! bench_unop {
+    ($name: ident, $desc: expr, op => $unop: ident, ty => $t: ty, from => $from: ty) => {
+        pub(crate) fn $name(c: &mut Criterion) {
+            use rand::{Rng, SeedableRng};
+            use rand_xoshiro::Xoshiro256Plus;
+            const LEN: usize = 1 << 13;
+
+            let mut rng = Xoshiro256Plus::seed_from_u64(0);
+
+            let elems: Vec<$t> = (0..LEN).map(|_| rng.gen::<$from>().into()).collect();
+            let mut i = 0;
+
+            c.bench_function($desc, move |b| {
+                b.iter(|| {
+                    i = (i + 1) & (LEN - 1);
+
+                    unsafe { criterion::black_box(elems.get_unchecked(i).$unop()) }
+                })
+            });
+        }
+    };
+
+    ($name: ident, $desc: expr, op => $unop: ident, ty => $t: ty) => {
+        bench_unop!($name, $desc, op => $unop, ty => $t, from => $t);
+    };
+}
+
+#[macro_export]
+macro_rules! bench_binop {
+    ($name: ident, $desc: expr, op => $binop: ident, ty1 => $ty1:ty, from1 => $from1:ty, ty2 => $ty2:ty, from2 => $from2:ty) => {
+        pub(crate) fn $name(c: &mut Criterion) {
+            use rand::{Rng, SeedableRng};
+            use rand_xoshiro::Xoshiro256Plus;
+            const LEN: usize = 1 << 7;
+
+            let mut rng = Xoshiro256Plus::seed_from_u64(0);
+
+            let elems1: Vec<$ty1> = (0..LEN).map(|_| rng.gen::<$from1>().into()).collect();
+            let elems2: Vec<$ty2> = (0..LEN).map(|_| rng.gen::<$from2>().into()).collect();
+            let mut i = 0;
+
+            c.bench_function($desc, move |b| {
+                for lhs in elems1.iter() {
+                    b.iter(|| {
+                        i = (i + 1) & (LEN - 1);
+                        unsafe {
+                            criterion::black_box(
+                                lhs.$binop(elems2.get_unchecked(i)),
+                                );
+                        }
+                    })
+                }
+            });
+        }
+    };
+
+    ($name: ident, $desc: expr, op => $binop: ident, ty => $t: ty, from => $from: ty) => {
+        bench_binop!($name, $desc, op => $binop, ty1 => $t, from1 => $from, ty2 => $t, from2 => $from);
+    };
+
+    ($name: ident, $desc: expr, op => $binop: ident, ty1 => $t1: ty, ty2 => $t2: ty) => {
+        bench_binop!($name, $desc, op => $binop, ty1 => $t1, from1 => $t1, ty2 => $t2, from2 => $t2);
+    };
+
+    ($name: ident, $desc: expr, op => $binop: ident, ty => $t: ty) => {
+        bench_binop!($name, $desc, op => $binop, ty1 => $t, from1 => $t, ty2 => $t, from2 => $t);
+    };
+}
