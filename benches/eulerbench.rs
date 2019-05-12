0828f5101a04dbddf59faa613bbf7e474bb6a692
Cameron Hart <cameron.hart@gmail.com>
2019-05-12 13:13:18 +1000

    Use macro for euler bench, add 2d version.

--- benches/eulerbench.rs
+++ benches/eulerbench.rs
@@ -1,101 +1,79 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use rand::{Rng, SeedableRng};
-use rand_xoshiro::Xoshiro256Plus;
+use criterion::{criterion_group, criterion_main};
 
 const UPDATE_RATE: f32 = 1.0 / 60.0;
 const NUM_OBJECTS: usize = 10000;
 
-fn cgmath_benchmark(c: &mut Criterion) {
-    use cgmath::{prelude::*, Vector3};
-    struct TestData {
-        acceleration: Vec<Vector3<f32>>,
-        velocity: Vec<Vector3<f32>>,
-        position: Vec<Vector3<f32>>,
-    }
-    let mut rng = Xoshiro256Plus::seed_from_u64(0);
-    let mut data = TestData {
-        acceleration: vec![rng.gen(); NUM_OBJECTS],
-        velocity: vec![Vector3::zero(); NUM_OBJECTS],
-        position: vec![Vector3::zero(); NUM_OBJECTS],
-    };
-    c.bench_function("cgmath_euler", move |b| {
-        b.iter(|| {
-            let dt = UPDATE_RATE;
-            for ((position, acceleration), velocity) in data
-                .position
-                .iter_mut()
-                .zip(&data.acceleration)
-                .zip(&mut data.velocity)
-            {
-                *velocity += *acceleration * dt;
-                *position += *velocity * dt;
+#[macro_export]
+macro_rules! euler {
+    ($name: ident, $desc: expr, ty => $t: ty, storage => $storage: ty, zero => $zero: expr) => {
+        pub(crate) fn $name(c: &mut Criterion) {
+            use rand::{Rng, SeedableRng};
+            use rand_xoshiro::Xoshiro256Plus;
+            use super::{NUM_OBJECTS, UPDATE_RATE};
+            struct TestData {
+                acceleration: Vec<$storage>,
+                velocity: Vec<$storage>,
+                position: Vec<$storage>,
             }
-        })
-    });
+            let mut rng = Xoshiro256Plus::seed_from_u64(0);
+            let mut data = TestData {
+                acceleration: vec![rng.gen(); NUM_OBJECTS],
+                velocity: vec![$zero; NUM_OBJECTS],
+                position: vec![$zero; NUM_OBJECTS],
+            };
+            c.bench_function($desc, move |b| {
+                b.iter(|| {
+                    let dt = UPDATE_RATE;
+                    for ((position, acceleration), velocity) in data
+                        .position
+                            .iter_mut()
+                            .zip(&data.acceleration)
+                            .zip(&mut data.velocity)
+                            {
+                                let local_acc: $t = (*acceleration).into();
+                                let mut local_pos: $t = (*position).into();
+                                let mut local_vel: $t = (*velocity).into();
+                                local_vel += local_acc * dt;
+                                local_pos += local_vel * dt;
+                                *velocity = local_vel.into();
+                                *position = local_pos.into();
+                            }
+                })
+            });
+        }
+    };
 }
 
-fn glam_benchmark(c: &mut Criterion) {
-    use glam::Vec3;
-    struct TestData {
-        acceleration: Vec<Vec3>,
-        velocity: Vec<Vec3>,
-        position: Vec<Vec3>,
-    }
-    let mut rng = Xoshiro256Plus::seed_from_u64(0);
-    let mut data = TestData {
-        acceleration: vec![rng.gen(); NUM_OBJECTS],
-        velocity: vec![Vec3::zero(); NUM_OBJECTS],
-        position: vec![Vec3::zero(); NUM_OBJECTS],
-    };
-    c.bench_function("glam_euler", move |b| {
-        b.iter(|| {
-            let dt = UPDATE_RATE;
-            for ((position, acceleration), velocity) in data
-                .position
-                .iter_mut()
-                .zip(&data.acceleration)
-                .zip(&mut data.velocity)
-            {
-                *velocity += *acceleration * dt;
-                *position += *velocity * dt;
-            }
-        })
-    });
+mod cgmath_bench {
+    use criterion::Criterion;
+    use cgmath::{prelude::*, Vector2, Vector3};
+    euler!(euler_3d, "cgmath euler 3d", ty => Vector3<f32>, storage => Vector3<f32>, zero => Vector3::zero());
+    euler!(euler_2d, "cgmath euler 2d", ty => Vector2<f32>, storage => Vector2<f32>, zero => Vector2::zero());
 }
 
-fn nalgebra_benchmark(c: &mut Criterion) {
-    use nalgebra_glm::{zero, Vec3};
-    struct TestData {
-        acceleration: Vec<Vec3>,
-        velocity: Vec<Vec3>,
-        position: Vec<Vec3>,
-    }
-    let mut rng = Xoshiro256Plus::seed_from_u64(0);
-    let mut data = TestData {
-        acceleration: vec![rng.gen(); NUM_OBJECTS],
-        velocity: vec![zero(); NUM_OBJECTS],
-        position: vec![zero(); NUM_OBJECTS],
-    };
-    c.bench_function("nalgebra_euler", move |b| {
-        b.iter(|| {
-            let dt = UPDATE_RATE;
-            for ((position, acceleration), velocity) in data
-                .position
-                .iter_mut()
-                .zip(&data.acceleration)
-                .zip(&mut data.velocity)
-            {
-                *velocity += *acceleration * dt;
-                *position += *velocity * dt;
-            }
-        })
-    });
+mod glam_bench {
+    use criterion::Criterion;
+    use glam::{Vec2, Vec3};
+    euler!(euler_3d_vec3, "glam euler 3d vec3", ty => Vec3, storage => Vec3, zero => Vec3::zero());
+    euler!(euler_3d_tuple, "glam euler 3d tuple", ty => Vec3, storage => (f32, f32, f32), zero => (0.0, 0.0, 0.0));
+    euler!(euler_2d_vec2, "glam euler 2d vec2", ty => Vec2, storage => Vec2, zero => Vec2::zero());
+}
+
+mod nalgebra_bench {
+    use criterion::Criterion;
+    use nalgebra_glm::{zero, Vec2, Vec3};
+    euler!(euler_3d, "nalgebra_glm euler 3d", ty => Vec3, storage => Vec3, zero => zero());
+    euler!(euler_2d, "nalgebra_glm euler 2d", ty => Vec2, storage => Vec2, zero => zero());
 }
 
 criterion_group!(
     benches,
-    cgmath_benchmark,
-    glam_benchmark,
-    nalgebra_benchmark
+    cgmath_bench::euler_3d,
+    cgmath_bench::euler_2d,
+    glam_bench::euler_3d_vec3,
+    glam_bench::euler_3d_tuple,
+    glam_bench::euler_2d_vec2,
+    nalgebra_bench::euler_3d,
+    nalgebra_bench::euler_2d,
 );
 criterion_main!(benches);
