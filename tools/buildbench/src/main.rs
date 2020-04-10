defacabb1f562708c5271602d308f0d3afc0416e
Cameron Hart <cameron.hart@gmail.com>
2020-04-10 15:23:57 +1200

    Added prettytable output.

--- tools/buildbench/src/main.rs
+++ tools/buildbench/src/main.rs
@@ -1,5 +1,6 @@
 use cargo_toml::Manifest;
 use clap::{App, Arg};
+use lazy_static::lazy_static;
 use regex::Regex;
 use std::{
     collections::HashSet,
@@ -28,10 +29,10 @@ fn create_temp_dir(name: &str, version: &str) -> io::Result<TempDir> {
     writeln!(file, "[package]")?;
     writeln!(file, "name = \"mathbench-{}-buildtime\"", name)?;
     writeln!(file, "version = \"0.1.0\"")?;
-    writeln!(
-        file,
-        "authors = [\"Cameron Hart <cameron.hart@gmail.com>\"]"
-    )?;
+    // writeln!(
+    //     file,
+    //     "authors = [\"Cameron Hart <cameron.hart@gmail.com>\"]"
+    // )?;
     writeln!(file, "edition = \"2018\"")?;
     writeln!(file, "[dependencies]")?;
     writeln!(file, "{} = \"{}\"", name, version)?;
@@ -42,7 +43,7 @@ fn create_temp_dir(name: &str, version: &str) -> io::Result<TempDir> {
 
     let src_path = src_dir_path.join("lib.rs");
     let mut file = File::create(src_path)?;
-    writeln!(file, "")?;
+    writeln!(file)?;
 
     Ok(dir)
 }
@@ -67,46 +68,77 @@ fn build_crate(name: &str, dir: TempDir) -> io::Result<TimingInfo> {
     let mut timing_info = TimingInfo::default();
     timing_info.name = name.to_string();
 
-    let completed_match = Regex::new(r"^\s*Completed ([\S]+) ([\S]+) in ([\S]+)").unwrap();
-    let finished_match = Regex::new(r"\s*Finished ([\S]+) \[.+\] target\(s\) in ([\S]+)").unwrap();
+    lazy_static! {
+        static ref COMPLETED_MATCH: Regex =
+            Regex::new(r"^\s*Completed ([\S]+) ([\S]+) in ([\S]+)").unwrap();
+        static ref FINISHED_MATCH: Regex =
+            Regex::new(r"\s*Finished ([\S]+) \[.+\] target\(s\) in ([\S]+)").unwrap();
+    }
 
     for line in reader.lines() {
         let line = line?;
         println!("{}", line);
-        if let Some(completed_captures) = completed_match.captures(line.as_str()) {
+        if let Some(completed_captures) = COMPLETED_MATCH.captures(line.as_str()) {
             timing_info.units += 1;
             let completed_name = completed_captures.get(1).unwrap().as_str();
             if name == completed_name {
                 timing_info.self_time = parse_time(completed_captures.get(3).unwrap().as_str());
             }
-        } else if let Some(finished_captures) = finished_match.captures(line.as_str()) {
+        } else if let Some(finished_captures) = FINISHED_MATCH.captures(line.as_str()) {
             timing_info.profile = finished_captures.get(1).unwrap().as_str().to_string();
             timing_info.total_time = parse_time(finished_captures.get(2).unwrap().as_str());
         }
     }
 
-    dbg!(&timing_info);
-
-    // let output: Vec<String> = reader.lines().filter_map(|line| line.ok()).inspect(|line| println!("{}", line)).collect();
-
     // copy timing file
     let current_dir = env::current_dir()?;
     let timing_html = format!("cargo-timing-{}.html", name);
-    fs::copy(dir.path().join("cargo-timing.html"), current_dir.join(timing_html))?;
+    fs::copy(
+        dir.path().join("cargo-timing.html"),
+        current_dir.join(timing_html),
+    )?;
 
     Ok(timing_info)
 }
 
-fn benchlib(target_name: &str, target_version: &str) -> io::Result<()> {
+fn benchlib(target_name: &str, target_version: &str) -> io::Result<TimingInfo> {
     let dir = create_temp_dir(target_name, target_version)?;
+    build_crate(target_name, dir)
+}
 
-    let _output = build_crate(target_name, dir)?;
+fn summarize(results: &[TimingInfo]) {
+    use prettytable::{
+        format::{Alignment, FormatBuilder, LinePosition, LineSeparator},
+        Cell, Row, Table,
+    };
 
-    // let timing_infos = collect_timing_info(output)?;
+    let markdown_format = FormatBuilder::new()
+        .padding(1, 1)
+        .borders('|')
+        .separator(LinePosition::Title, LineSeparator::new('-', '|', '|', '|'))
+        .column_separator('|')
+        .build();
 
-    // summarize_timing_infos(target_name, &timing_infos);
+    let mut table = Table::new();
+    table.set_format(markdown_format);
 
-    Ok(())
+    table.set_titles(Row::new(vec![
+        Cell::new_align("name", Alignment::LEFT),
+        Cell::new_align("total (s)", Alignment::RIGHT),
+        Cell::new_align("self (s)", Alignment::RIGHT),
+        Cell::new_align("units", Alignment::RIGHT),
+    ]));
+
+    for info in results {
+        table.add_row(Row::new(vec![
+            Cell::new_align(info.name.as_str(), Alignment::LEFT),
+            Cell::new_align(&format!("{:.1}", info.total_time), Alignment::RIGHT),
+            Cell::new_align(&format!("{:.1}", info.self_time), Alignment::RIGHT),
+            Cell::new_align(&format!("{}", info.units), Alignment::RIGHT),
+        ]));
+    }
+
+    table.printstd();
 }
 
 fn main() {
@@ -148,10 +180,17 @@ fn main() {
         .values_of("lib")
         .map_or(HashSet::new(), |v| v.collect());
 
-    for (name, version) in lib_pairs {
-        if allowed_libs.is_empty() || allowed_libs.contains(name) {
-            println!("Building {} {}", name, version);
-            benchlib(name, version).unwrap();
-        }
-    }
+    let results: Vec<TimingInfo> = lib_pairs
+        .iter()
+        .filter_map(|(name, version)| {
+            if allowed_libs.is_empty() || allowed_libs.contains(name) {
+                println!("Building {} {}", name, version);
+                benchlib(name, version).ok()
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    summarize(&results);
 }
