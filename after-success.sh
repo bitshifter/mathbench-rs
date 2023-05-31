187e7f8e839b23b2e3ff8c3ee0b7cca429b72bf9
Kaushik.J.G <amit2ghumaliya@gmail.com>
2023-05-31 23:33:44 +0530

    change made in after-success.sh

--- after-success.sh
+++ after-success.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 # Clone the repository
-REMOTE_URL="$(git config --get remote.origin.url)";
-cd ${TRAVIS_BUILD_DIR}/.. && \
-git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench" && \
-cd  "${TRAVIS_REPO_SLUG}-bench" && \
+REMOTE_URL="$(git config --get remote.origin.url)"
+cd $GITHUB_WORKSPACE/.. &&
+git clone $REMOTE_URL "${GITHUB_REPOSITORY}-bench" &&
+cd "${GITHUB_REPOSITORY}-bench" &&
 
 # Bench master
-git checkout master && \
-cargo bench --bench benchmark -- --noplot --save-baseline before && \
+git checkout main &&  # Replace 'master' with 'main'
+cargo bench --bench benchmark -- --noplot --save-baseline before &&
 
 # Bench current branch
-git checkout ${TRAVIS_COMMIT} && \
-cargo bench --bench benchmark -- --noplot --save-baseline after && \
+git checkout $GITHUB_SHA &&  # Replace '${TRAVIS_COMMIT}' with '$GITHUB_SHA'
+cargo bench --bench benchmark -- --noplot --save-baseline after &&
 
 # Install https://github.com/BurntSushi/critcmp
-cargo install critcmp --force && \
+cargo install critcmp --force &&
 
 # Compare the two generated benches
 critcmp before after;
